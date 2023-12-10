678fde3d5ec6c9d43786b246826371e6c13e57b3
Erich Focht <efocht@gmail.com>
2023-12-10 22:13:21 +0900

    Fixes

--- sgemv_cmo_omp2.c
+++ sgemv_cmo_omp2.c
@@ -12,7 +12,7 @@ void sgemv_cmo_omp2(float* xout, float* x, bfloat16* w, int n, int d) {
             int block = (n + nthr - 1) / nthr;
             int nmin = ithr * block;
             int nmax = (ithr + 1) * block > n ? n : (ithr + 1) * block;
-            sgemv_bf16_cmo_n(&tmp, x, &w[imin], n, d, nmin, nmax, i, dmax);
+            sgemv_bf16_cmo_n(&tmp, x, &w[nmin], n, d, nmin, nmax, i, dmax);
         }
         for (int ithr = 0; ithr < nthr; ithr++) {
             for (int ii = i; ii < dmax; ii++)
